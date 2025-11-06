1a5b0b22b55e655e6040851d27ea82bb560efdce
OliverJiang2025 <sitpole@outlook.com>
2025-11-06 17:21:32 +0000

    hi

--- Q1.py
+++ Q1.py
@@ -2,14 +2,26 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy
-from plot_ksdensity import *
+#from plot_ksdensity import ksdensity
 
-N = 0
-num_bins = 0
+N = 1000
+num_bins = 30
 
 def plot_data(N, num_bins):
     # plot gaussian data and uniform data with length N
     # in a histogram with bins number of num_bins
-    guassian_data = np.random.normal(0, 1, N)
-    unif_data = np.random.uniform(0, 1, N)
+    gaussian_data = np.random.normal(0, 1, N)
+    unif_data = np.random.uniform(1, 0, N)
+    #print(Gaussian_data)
 
+    plt.subplot(1,2,1)
+    plt.hist(gaussian_data, bins = num_bins)
+
+
+
+    plt.subplot(1,2,2)
+    plt.show()
+
+
+
+plot_data(N, num_bins)
